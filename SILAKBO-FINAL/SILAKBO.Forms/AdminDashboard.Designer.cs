768b79aecbcd1b47ac74d299b70272d6cdbcbf19
villanuevaahdlei6-sketch <villanuevaahdlei6@gmail.com>
2026-03-19 20:27:39 +0800

    update admin dashboard design

--- SILAKBO-FINAL/SILAKBO.Forms/AdminDashboard.Designer.cs
+++ SILAKBO-FINAL/SILAKBO.Forms/AdminDashboard.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminDashboard";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboard));
+            pictureBox1 = new PictureBox();
+            btnRefresh = new Button();
+            dgvSupport = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvSupport).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 50);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(818, 78);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(75, 23);
+            btnRefresh.TabIndex = 4;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // dgvSupport
+            // 
+            dgvSupport.BackgroundColor = Color.White;
+            dgvSupport.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvSupport.Location = new Point(59, 107);
+            dgvSupport.Name = "dgvSupport";
+            dgvSupport.Size = new Size(834, 439);
+            dgvSupport.TabIndex = 3;
+            // 
+            // AdminDashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(964, 591);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnRefresh);
+            Controls.Add(dgvSupport);
+            Name = "AdminDashboard";
+            Text = "AdminDashboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvSupport).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnRefresh;
+        private DataGridView dgvSupport;
     }
 }
