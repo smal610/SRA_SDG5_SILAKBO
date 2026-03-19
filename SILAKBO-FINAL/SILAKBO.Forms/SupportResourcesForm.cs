3b10441e7fac6255d872bcffdf4ab6f26118b4fd
Sayno16 <423002589@ntc.edu.ph>
2026-03-19 21:08:53 +0800

    added support resources code

--- SILAKBO-FINAL/SILAKBO.Forms/SupportResourcesForm.cs
+++ SILAKBO-FINAL/SILAKBO.Forms/SupportResourcesForm.cs
@@ -10,9 +10,15 @@ namespace SILAKBO_FINAL.SILAKBO.Forms
 {
     public partial class SupportResourcesForm : Form
     {
+        private SupportRepository supportRepo = new SupportRepository();
+
         public SupportResourcesForm()
         {
             InitializeComponent();
+            LoadContacts();
         }
+
+        private void LoadContacts() => dgvSupport.DataSource = supportRepo.GetAllContacts();
+        private void btnRefresh_Click(object sender, EventArgs e) => LoadContacts();
     }
 }
